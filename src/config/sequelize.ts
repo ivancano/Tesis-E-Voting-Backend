d5db4d652a9dd51685a9c5fe50f46c4c0fde6431
Ivan Cano <ivan.cano92@gmail.com>
2021-11-13 20:42:47 -0300

    db

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,13 +1,13 @@
 import { Sequelize } from 'sequelize'
 
 const db = process.env.DB_DATABASE
+console.log(db);
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
 
 export const sequelize = new Sequelize(db, username, password, {
   dialect: "mysql",
-  port: 3306,
-  host: process.env.DB_HOST
+  port: 3306
 });
 
 sequelize.authenticate()
